669646c0e40deb6988b3aea43fd97c907265aa9b
Andrew Rivera <andrewmarcrivera@gmail.com>
2021-04-09 11:45:04 -0400

    Basic card structure laid out

--- src/index.js
+++ src/index.js
@@ -9,6 +9,33 @@ searchForm.addEventListener('submit', function () {
 
   const response = requestWeatherData(searchText);
   response.then(data => {
-    console.log(data.main.temp);
+    updateWeatherData(data);
+    console.log(data);
   });
 });
+
+function updateWeatherData (data) {
+  const cityName = document.getElementById('city-name');
+  cityName.innerHTML = data.name;
+
+  const mainTemp = document.getElementById('main-temp');
+  mainTemp.innerHTML = data.main.temp;
+
+  const weatherMain = document.getElementById('weather-main');
+  weatherMain.innerHTML = data.weather[0].main;
+
+  const weatherDesc = document.getElementById('weather-desc');
+  weatherDesc.innerHTML = data.weather[0].description;
+
+  const highTemp = document.getElementById('high-temp');
+  highTemp.innerHTML = data.main.temp_max;
+
+  const lowTemp = document.getElementById('low-temp');
+  lowTemp.innerHTML = data.main.temp_min;
+
+  const feelsLike = document.getElementById('feels-like');
+  feelsLike.innerHTML = data.main.feels_like;
+
+  const humidity = document.getElementById('humidity');
+  humidity.innerHTML = data.main.humidity;
+}
